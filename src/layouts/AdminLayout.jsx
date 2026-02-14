8f957b957ea422944d32c77992ccfa34fb9f6c82
silambu c <144410442+c-silambu@users.noreply.github.com>
2026-02-14 20:11:58 +0530

    Update AdminLayout.jsx

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -2,12 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/sidebar/Sidebar";
 import Topbar from "../components/topbar/Topbar";
-<<<<<<< HEAD
-import "./AdminLayout.css";
-=======
 import "./adminLayout.css";
->>>>>>> 4969e58 (Fixed InvoiceCreate import path issue)
-
 export default function AdminLayout() {
   return (
     <div className="admin-layout">
